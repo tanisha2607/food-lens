30c6ce68d9992b4cc778513dc2688c368ab7e7e8
Tanisha <tanishag.2607@email.com>
2026-05-30 15:36:47 +0530

    made config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+import torch
+import os
+
+BATCH_SIZE = 32
+NUM_CLASSES = 101
+NUM_EPOCHS = 10
+LEARNING_RATE = 0.001
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+DATA_DIR = os.path.join(os.getcwd(), "data")
+CHECKPOINT_DIR = os.path.join(os.getcwd(), "checkpoints")
